75b4fb9d450cfe03f4826969794fb4003e9b5c79
shad <shadab.ahmed63@gmail.com>
2018-11-28 20:35:00 +0000

    api basic test

--- ariana/quest/tests.py
+++ ariana/quest/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from .models import *
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from quest.serializers import QuestionnaireSerializer
+
 # Create your tests here.
 
-class QuestionnareModelTest(TestCase):
+class QuestionnareTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -20,4 +25,32 @@ class QuestionnareModelTest(TestCase):
         expected_object_slug = f'{questionnaire.slug}'
         self.assertEquals(expected_object_slug, 'Test-Questionnaire-2')
 
+class BaseViewTest(APITestCase):
+    client = APIClient()
 
+    @staticmethod
+    def create_questionnaire(name="", slug=""):
+        if name != "" and slug != "":
+            Questionnaire.objects.create(name=name, slug=slug)
+
+    def setUp(self):
+        # add test data
+        self.create_questionnaire("Are you hungry ?", "Are-you-hungry")
+        self.create_questionnaire("simple list", "simple-list")
+       
+
+
+class GetAllQuestionnairesTest(BaseViewTest):
+
+    def test_get_all_questionnaire(self):
+        """
+        This test ensures that all Questionnaire added in the setUp method
+        exist when we make a GET request to the Questionnaire/ endpoint
+        """
+        # hit the API endpoint
+        response = self.client.get("/api/questionnaire/")
+        # fetch the data from db
+        expected = Questionnaire.objects.all()
+        serialized = QuestionnaireSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
